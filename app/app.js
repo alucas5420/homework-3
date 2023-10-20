3b633c4578f11ad55c9434ead63de67cf389a009
alucas5420 <112018521+alucas5420@users.noreply.github.com>
2023-10-20 14:57:27 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,145 @@
+function initListeners() {
+    $(".bars").click(function(e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+
+    $(".links a").click(function(e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+}
+
+$(document).ready(function (){
+    initListeners();
+});
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn2").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn3").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn4").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn5").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn6").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn7").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn8").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn9").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("myBtn10").addEventListener("click", function() {
+    alert("View");
+  });
+
+  document.getElementById("editBtn").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn2").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn3").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn4").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn5").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn6").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn7").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn8").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn9").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("editBtn10").addEventListener("click", function() {
+    alert("Edit");
+  });
+
+  document.getElementById("delBtn").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+  document.getElementById("delBtn2").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn3").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn4").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn5").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn6").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn7").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn8").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn9").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
+  document.getElementById("delBtn10").addEventListener("click", function() {
+    alert("Delete");
+  });
+
+
